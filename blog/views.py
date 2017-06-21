864e12ccf6fff51d9c64556d33936d7fa59aa387
pascal <pascal@pascal-iMac>
2017-06-21 22:27:06 +0200

    Premier commit

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    """ Exemple de page HTML, non valide pour que l'exemple soit concis """
+    text = """<h1>Bienvenue sur mon blog !</h1>
+              <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>"""
+    return HttpResponse(text)
